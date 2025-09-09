3dad960cd2534dfd2a6706fbb7c038d453226357
joaovic <joaovictorca2004@gmail.com>
2025-09-09 12:28:28 -0300

    refatorando

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,17 +1,5 @@
-import { HeroSection } from "@/components/hero-section";
-import { FeatureSection } from "@/components/feature-section";
-import {SupportSection} from "@/components/support-section";
-import { CustomerStorySection } from "@/components/customer-story-section";
-import { CallToAction } from "@/components/call-to-action";
+import { LandingPage } from "@/templates/landing-page/landing-page";
 
 export default function Home() {
-  return (
-    <article className="flex-col">
-      <HeroSection />
-      <FeatureSection />
-      <SupportSection />
-      <CustomerStorySection />
-      <CallToAction />
-    </article>
-  );
+  return <LandingPage />;
 }
--- site-blog/src/templates/landing-page/sections/call-to-action/call-to-action.tsx
+++ site-blog/src/templates/landing-page/sections/call-to-action/call-to-action.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
 import { ArrowRight, Store } from "lucide-react";
-import { Button } from "../ui/button";
 import Link from "next/link";
 
 export const CallToAction = () => {
@@ -7,12 +7,12 @@ export const CallToAction = () => {
     <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
       <div className="absolute inset-0 hidden md:block bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
 
+      <div className="absolute top-0 left-1/2 -translate-y-1/2 -translate-x-1/2 p-4 bg-cyan-300 w-fit rounded-full">
+        <Store className="text-cyan-100" />
+      </div>
+
       <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
-          <div className="p-4 bg-cyan-300 w-fit rounded-full">
-            <Store className="text-cyan-100" />
-          </div>
-
           <h2 className="text-heading-xl text-balance text-gray-100 font-sans">
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
