d9badc9ea4de990e1def012dd117d0d60a957b63
joaovic <joaovictorca2004@gmail.com>
2025-09-09 17:24:37 -0300

    Criando a funcionalidade de busca

--- site-blog/src/templates/blog/components/post-card/post-card.tsx
+++ site-blog/src/templates/blog/components/post-card/post-card.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 type Author = {
   name: string;
@@ -38,7 +38,7 @@ export const PostCard = ({
           </div>
           <Image
             src={image}
-            alt=""
+            alt={title}
             width={288}
             height={144}
             className="w-full h-40 object-cover object-center rounded-t-[8px]"
