66722a2f6e30e9e798c77a612b001ba374dbdc5f
joaovic <joaovictorca2004@gmail.com>
2025-09-09 10:10:17 -0300

    section suport

--- site-blog/src/pages/index.tsx
+++ site-blog/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/components/hero-section";
 import { FeatureSection } from "@/components/feature-section";
+import {SupportSection} from "@/components/support-section";
 
 export default function Home() {
   return (
     <article className="flex-col">
       <HeroSection />
       <FeatureSection />
+      <SupportSection />
     </article>
   );
 }
