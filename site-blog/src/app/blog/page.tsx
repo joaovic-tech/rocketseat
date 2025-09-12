83518ba5a8010eec3ec495dea47721e4e9c8f088
joaovic <joaovictorca2004@gmail.com>
2025-09-12 11:26:15 -0300

    add SEO na blog

--- site-blog/src/app/blog/page.tsx
+++ site-blog/src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from "@/templates/blog";
-import {allPosts} from "contentlayer/generated";
+import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://site-blog-rocketseat.vercel.app/og-image.png",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-rocketseat.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogPage() {
   const sortedPosts = allPosts.sort(
