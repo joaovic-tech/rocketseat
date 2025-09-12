b60643bbf8233bc1b851815e9157c88e59df4cd9
joaovic <joaovictorca2004@gmail.com>
2025-09-12 11:18:29 -0300

    add SEO na landing page

--- site-blog/src/app/layout.tsx
+++ site-blog/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" suppressHydrationWarning={true}>
       <body>
         <Layout>{children}</Layout>
       </body>
--- site-blog/src/app/page.tsx
+++ site-blog/src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-rocketseat.vercel.app/og-image.png",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-rocketseat.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Site.set",
+      },
+    ],
+  },
+};
 
 export default function Page() {
   return <LandingPage />;
